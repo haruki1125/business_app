3a350781224da667296b19224e99f6927151b34a
haruki1125 <haruki1125@users.noreply.github.com>
2021-01-13 13:07:15 +0900

    commit

--- app/assets/javascripts/channels/notes/note.js
+++ app/assets/javascripts/channels/notes/note.js
@@ -1,5 +1,5 @@
 function note() {
-  const submit = document.getElementById("submit");
+  const submit = document.getElementById("input");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
